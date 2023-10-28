9c794d39df2295d339b1d2509905fce3954774e2
agisbertb <agisbert@iesebre.com>
2023-10-28 09:56:58 +0200

    ResetGame fet

--- app/src/main/java/me/andreugisbert/dam/comptador/MainActivity.kt
+++ app/src/main/java/me/andreugisbert/dam/comptador/MainActivity.kt
@@ -18,15 +18,18 @@ import me.andreugisbert.dam.comptador.ui.theme.ComptadorTheme
 
 class MainActivity : ComponentActivity() {
 
+    private val INITIAL_TIME = 20
+
     internal lateinit var tapMeButton : Button
     internal lateinit var timeTextView : TextView
     internal lateinit var counterTextView : TextView
     internal var counter = 0
-    internal var time = 60
+    internal var time = INITIAL_TIME
 
     internal var appStarted = false
     internal lateinit var countdownTimer : CountDownTimer
-    internal val initialCountDownTimer: Long = 60000
+    //internal val initialCountDownTimer: Long = 60000
+    internal val initialCountDownTimer: Long = time.toLong() *1000
     internal val intervalCountDownTimer: Long = 1000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -70,11 +73,21 @@ class MainActivity : ComponentActivity() {
     }
 
     private fun endGame() {
-        Toast.makeText(this,getString(R.string.endGame),Toast.LENGTH_LONG).show()
-//            resetGame()
+        Toast.makeText(this,getString(R.string.endGame, counter), Toast.LENGTH_LONG).show()
+            resetGame()
+    }
+    private fun resetGame() {
+        // RESET PUNTUACIÓ A ZERO
+        counter = 0
+        counterTextView.text = counter.toString()
+        // REINICIALITZAR EL COMPTADOR
+        time = INITIAL_TIME
+        timeTextView.text = time.toString()
+        initCountdown()
+
+        // GAME STARTED A FALSE
+        appStarted = false
     }
 }
 
-    private fun resetGame() {
-        //TODO
-    }
+
